26acf0228e87f6f1cf6d078784b04ff7184bd6b3
Cloud User <ec2-user@ip-172-31-36-9.eu-central-1.compute.internal>
2019-05-26 13:59:58 +0000

    test

--- app/main.py
+++ app/main.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "David Hello World from Flask"
-@app.route("/david")
-def david():
-    return "Hello, david"
+#@app.route("/david")
+#def david():
+#    return "Hello, david"
 
 if __name__ == "__main__":
     # Only for debugging while developing
